928e51eae4021cc028d410660dc5ad0988994cd9
ChanaIrinshtein <150062394+ChanaIrinshtein@users.noreply.github.com>
2024-03-28 09:04:02 +0000

    tests for gabais contract

--- test/wallet/CollectorsWallet.t.sol
+++ test/wallet/CollectorsWallet.t.sol
@@ -40,40 +40,7 @@ contract CollectorsTest is Test {
         vm.stopPrank();
     }
 
-    function testWithdraw() public {
-        require(address(this).balance>=50, "fff");
-        uint amount = 50;
-        uint256 startBalance = address(this).balance;
-        console.log(startBalance);
-        //finalBalance = address(wallet).balance;
-        uint finalBalance = address(this).balance;
-        console.log(address(this).balance);
-        finalBalance = wallet.withdraw(amount);
-        console.log(address(this).balance);
-        assertEq(finalBalance, startBalance-amount);
-         console.log(finalBalance);
-    }
+   
 
-    // function testUpdateCollectors() public {
-    //     address oldCollector = 0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d;
-    //     address newCollector = 0x7c0FA5571c4A1A67FD21Ed9209674868cC8dc86b;
-    //     address userAddress = 0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f; // address of allowed user
-    //     // address userAddress = vm.addr(1); // address of not allowed user
-    //     vm.startPrank(userAddress); // send from random address
-    //     vm.expectRevert();
-    //     wallet.updateCollectors(oldCollector, newCollector);
-    //     // vm.startPrank(userAddress); // send from random address
-    //     // vm.expectRevert();
-    //     // walletC.updateCollectors(oldCollector, newCollector);
-    //     // why it is not work?
-    //     // assertEq(wallet.collectors(newCollector), 1);
-    //     // assertEq(wallet.owner() , 0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f);
-    //     // assertEq(wallet.collectors(oldCollector), 0);
-    //     vm.stopPrank();
-    // }
-    // function testGetBalance() public {
-    //     // why it is not work?
-    //     // assertEq(walletC.getBalance(), 50 , "not equals");
-    //     assertEq(wallet.getBalance(), address(wallet).balance, "not equals");
-    // }  
+
 }
