a1b3cbc9e53a07eb95b4790edba05b6d2de74a56
chana26252 <chana26252@gmail.com>
2024-05-06 20:13:17 +0300

    continue amm

--- src/AMM/amm.sol
+++ src/AMM/amm.sol
@@ -14,10 +14,11 @@ contract Amm {
     uint256 public balanceA;
     uint256 public balanceB;
     uint256 sum = 100 * wad;
+
     constructor(address _tokenA, address _tokenB) {
         tokenA = MyToken(_tokenA);
         tokenB = MyToken(_tokenB);
-        address owner = msg.sender;
+        // address owner = msg.sender;
         tokenA.mint(address(this), sum);
         tokenB.mint(address(this), sum);
         balanceA += sum;
@@ -35,6 +36,35 @@ contract Amm {
         }
         return (_balanceA * 1e18 / _balanceB) * amount / 1e18;
     }
+
+    // function calcLiquidity(uint256 _balanceA, uint256 _balanceB, uint256 amount, uint256 kindOfToken) public returns(uint256){
+    //     if (_balanceA > _balanceB) {
+    //         uint256 x = _balanceA * 1e18 / _balanceB / 1e18;
+    //         uint256 relativityB = x;
+    //         kindOfToken == 1}
+        //     if (kindOfToken == 1) {
+        //         uint256 resultA = amount;
+        //         uint256 resultB = amount * x;
+        //     }
+        //     else{
+        //         uint256 resultB = amount;
+        //         uint256 resultA = amount * x;
+        //     }
+        //        return resultA;
+        // } else {
+        //     uint256 x = _balanceA * 1e18 / _balanceB / 1e18;
+        //     uint256 relativityB = x;
+        //     if (kindOfToken == 2) {
+        //         uint256 resultA = amount;
+        //         uint256 resultB = amount * x;
+        //     }
+        //     else{
+        //         uint256 resultB = amount;
+        //         uint256 resultA = amount * x;
+        //     }
+        //        return resultB;
+        // }
+    // }
 
     function tradeAToB(uint256 amount) external {
         require(amount > 0, "amount is zero");
@@ -57,4 +87,22 @@ contract Amm {
         tokenA.transfer(msg.sender, balance);
         balanceA -= amount;
     }
+
+    // function AddLiquidityA(uint256 amount) external {
+    //     console.log("sum", amount / wad);
+    //     console.log("111111", address(this));
+    //     require(tokenA.balanceOf(msg.sender) / wad > amount / wad, "you dont have engouh amount in your account");
+    //     console.log("ssssssssssss", tokenA.balanceOf(msg.sender) / wad);
+    //     uint256 balance = calcLiquidity(500 * wad, 100 * wad, 20 * wad, 1) / wad;
+    //     console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&",balance);
+    //     // uint256 balance = calcCount(balanceA, balanceB, amount, 1) / 1e18;
+    //     // console.log("bbbbbbbbbbbbbbbbbbb",balance);
+    //     // tokenA.transferFrom(msg.sender, address(this), amount);
+    //     // balanceA += amount;
+    //     // uint256 balance = calcCount(balanceA, balanceB, amount, 1) / 1e18;
+    //     // console.log("balance:    ", balance);
+    //     // tokenB.transferFrom(msg.sender, address(this), balance);
+    //     // balanceB += amount;
+    //     // liquidity[msg.sender] += amount;
+    // }
 }
--- test/AMM/amm.t.sol
+++ test/AMM/amm.t.sol
@@ -17,23 +17,47 @@ contract TestAmm is Test {
         tokenB = new MyToken();
         amm = new Amm(address(tokenA), address(tokenB));
     }
+
     function testCalcCount() public {
         console.log(amm.calcCount(60 * wad, 50 * wad, 10 * wad, 1) / wad);
         console.log(amm.calcCount(60 * wad, 50 * wad, 10 * wad, 2) / wad);
         assertEq(amm.calcCount(60 * wad, 50 * wad, 10 * wad, 1), 8333333333333333330);
         assertEq(amm.calcCount(60 * wad, 50 * wad, 10 * wad, 2), 12000000000000000000);
     }
+
+    // function testCalcLiquidity() public {
+    //     console.log(amm.calcLiquidity(500 * wad, 100 * wad, 20 * wad, 1) / wad);
+    //     // console.log("gggggggg");
+    // }
+
     function testTradeAToB() external {
         uint256 amount = 50 * wad;
         tokenA.mint(address(this), amount);
         tokenA.approve(address(amm), amount);
         tokenB.mint(address(amm), amount);
         amm.tradeAToB(amount);
-    } function testTradeBToA() external {
+    }
+
+    function testTradeBToA() external {
         uint256 amount = 50 * wad;
         tokenB.mint(address(this), amount);
         tokenB.approve(address(amm), amount);
         tokenA.mint(address(amm), amount);
         amm.tradeBToA(amount);
     }
+
+    function testAddLiquidityA() external {
+        uint256 sum = 20 * wad;
+        uint256 amount = 50 * wad;
+        address user = vm.addr(123);
+        vm.startPrank(user);
+        console.log("msgmsg", user);
+        tokenA.approve(address(user), amount);
+        tokenA.mint(address(user), amount);
+        tokenB.approve(address(user), amount);
+        tokenB.mint(address(user), amount);
+        amm.AddLiquidityA(sum);
+        // console.log(calcLiquidity())
+    }
+
 }
